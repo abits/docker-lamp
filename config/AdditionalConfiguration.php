fa9154914b661aa29c53b280c3d14e0056ef028c
Christoph Martel <chris@codeways.org>
2016-09-12 23:04:09 +0200

    make typo3 complain less

--- config/AdditionalConfiguration.php
+++ config/AdditionalConfiguration.php
@@ -1,6 +1,14 @@
 <?php
-	$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = getenv("MYSQL_ENV_MYSQL_DATABASE");
-	$GLOBALS['TYPO3_CONF_VARS']['DB']['host']     = getenv("MYSQL_PORT_3306_TCP_ADDR");
-	$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = getenv("MYSQL_ENV_MYSQL_USER");
-	$GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = getenv("MYSQL_ENV_MYSQL_PASSWORD");
-?>
+	/**
+	 * Here we extract the values from our docker environment and override
+	 * the TYPO3 configuration. 
+	 */
+	$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = getenv('MYSQL_ENV_MYSQL_DATABASE');
+	$GLOBALS['TYPO3_CONF_VARS']['DB']['host']     = getenv('MYSQL_PORT_3306_TCP_ADDR');
+	$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = getenv('MYSQL_ENV_MYSQL_USER');
+	$GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = getenv('MYSQL_ENV_MYSQL_PASSWORD');
+
+	/**
+	 * Extra configuration in order to make TYPO3 complain less.
+	 */
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLocale'] = 'de_DE.UTF-8';
